be68b0e2ebac1471d6c7f5054c3adcb4e6722beb
rohman <sector.gamma@gmail.com>
2017-05-09 10:55:47 +0700

    notification fix

--- app/src/main/java/com/koceeng/freedonation/alarm/AlarmReceiver.java
+++ app/src/main/java/com/koceeng/freedonation/alarm/AlarmReceiver.java
@@ -2,12 +2,18 @@ package com.koceeng.freedonation.alarm;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
 import com.koceeng.freedonation.R;
+import com.koceeng.freedonation.home.SplashActivity;
+import com.koceeng.freedonation.util.LanguageUtil;
 
 public class AlarmReceiver extends WakefulBroadcastReceiver {
 
@@ -15,13 +21,27 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.e("AlarmReceiver", "onReceive: ");
 
+        LanguageUtil.getInstance().updateLanguageResource(context);
+
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setContentTitle(context.getString(R.string.setting_notification_text_title));
-        builder.setContentText(context.getString(R.string.setting_notification_text_content));
-        builder.setSmallIcon(R.drawable.icon_vector);
+        Intent intentNotification = new Intent(context, SplashActivity.class);
+        intentNotification.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentNotification,
+                PendingIntent.FLAG_ONE_SHOT);
 
-        notificationManager.notify(0, builder.build());
+        Notification notification = new NotificationCompat.Builder(context)
+                .setContentTitle(context.getString(R.string.setting_notification_text_title))
+                .setContentText(context.getString(R.string.setting_notification_text_content))
+                .setSmallIcon(R.drawable.icon_vector)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setContentIntent(pendingIntent)
+                .build();
+
+        notificationManager.notify(0, notification);
     }
 }
